3841d008869a7bc864cb79b30eac02fd5fa66193
Nolan Jacobson <nolanjacobson@protonmail.com>
2024-11-19 17:06:03 -0500

    chore: remove todo comments

--- cli/scripts/setup-deposit-authority.sh
+++ cli/scripts/setup-deposit-authority.sh
@@ -1,7 +1,6 @@
 STAKE_POOL_KEYPAIR_PATH="./keys/stake-pool.json"
 FEE_WALLET_KEYPAIR="./keys/identity_1.json"
 
-# TODO update authority keypair to be that of the cli config manager?
 AUTHORITY_KEYPAIR=$(solana config get | grep "Keypair Path" | awk '{print $3}')
 
 if [ ! -f "$STAKE_POOL_KEYPAIR_PATH" ]; then
@@ -24,5 +23,3 @@ AUTHORITY=$(solana-keygen pubkey "$AUTHORITY_KEYPAIR")
 
 # create stake_deposit_authority and set pubkey to var
 STAKE_DEPOSIT_AUTHORITY=$(../target/debug/spl-stake-pool-interceptor interceptor create-stake-deposit-authority --pool $STAKE_POOL --fee-wallet $FEE_WALLET --authority $AUTHORITY_KEYPAIR --cool-down-seconds 200 --initial-fee-bps 100 | tail -1)
-
-# TODO update stake pool stake_deposit_authority to $STAKE_DEPOSIT_AUTHORITY
