55d110c6c086aa4fd7b4c7dcdf7586e370267cdd
Taylor123 <nftaylor11@gmail.com>
2024-11-19 13:47:43 -0500

    nest deps in scripts

--- cli/scripts/setup-local-validator.sh
+++ cli/scripts/setup-local-validator.sh
@@ -7,8 +7,6 @@ transfer_to_wallet=$4
 transfer_to_amount=$5
 stake_per_validator=$((($sol_amount - ($max_validators * 2))/$max_validators))
 
-# `./scripts/setup-localnet.sh 10 local_validators.txt 50 $WALLET_TO_TRANSFER $LST_TRANSFER_AMOUNT`
-
 keys_dir=keys
 mkdir -p $keys_dir
 
@@ -62,8 +60,8 @@ increase_stakes () {
 
 setup_test_validator() {
   solana-test-validator \
-   --bpf-program SPoo1Ku8WFXoNDMHPsrGSTSG1Y47rzgn41SLUNakuHy ../../deps/stake_pool.so \
-   --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s ../../deps/mpl_metadata.so \
+   --bpf-program SPoo1Ku8WFXoNDMHPsrGSTSG1Y47rzgn41SLUNakuHy ./deps/stake_pool.so \
+   --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s ./deps/mpl_metadata.so \
    --slots-per-epoch 32 \
    --quiet --reset &
   pid=$!
